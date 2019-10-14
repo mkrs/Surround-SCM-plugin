d371a3ccbc2ee5b070f01612f2192abc152aacf5
mkrs <matthias.schwayer@andritz.com>
2019-10-14 06:56:52 +0200

    remove context and request

--- src/main/java/andritz/sscm/SurroundSCMRevision.java
+++ src/main/java/andritz/sscm/SurroundSCMRevision.java
@@ -1,6 +1,18 @@
 package andritz.sscm;
 
+import jenkins.scm.api.SCMHead;
+import jenkins.scm.api.SCMRevision;
+
 public class SurroundSCMRevision extends SCMRevision {
+   
+   /**
+    *
+    */
+   private static final long serialVersionUID = 1L;
+    
+   protected SurroundSCMRevision(SCMHead head) {
+      super(head);
+   }
 
    @Override
    public boolean equals(Object obj) {
